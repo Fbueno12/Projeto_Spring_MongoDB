99e4f206cd363e44de5b8ef47851e0eedd394deb
Felipe Bueno <felipebueno181@gmail.com>
2019-08-24 19:12:25 -0300

    Inserindo operação de insert de usuários

--- src/main/java/com/felipebueno/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/felipebueno/workshopmongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.felipebueno.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipebueno.workshopmongodb.domain.User;
 import com.felipebueno.workshopmongodb.dto.UserDTO;
@@ -35,6 +38,15 @@ public class UserResource {
 		UserDTO objdto = new UserDTO(obj);
 		
 		return ResponseEntity.ok().body(objdto);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj){
+		User user = service.fromDTO(obj);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
